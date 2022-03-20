4f1fc65c0fd11b134a479d569669e5ab2118938f
Raihan567 <aburaihansarker567@gmail.com>
2022-03-21 03:57:52 +0600

    added firebase file in working directory

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgjoIUELJalWrx9vJYV-AnbIFYtSEMo2U",
+  authDomain: "ema-john-authentication-c9eac.firebaseapp.com",
+  projectId: "ema-john-authentication-c9eac",
+  storageBucket: "ema-john-authentication-c9eac.appspot.com",
+  messagingSenderId: "669752714016",
+  appId: "1:669752714016:web:21d96603d65e0763dcad45",
+};
+
+export default firebaseConfig;
